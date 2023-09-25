12bb523ba38f84ea254ed08be2ba97ff99d0f8de
Sai Harish <30901561+Saihar1sh@users.noreply.github.com>
2023-09-25 19:42:30 +0530

    IK working

---
+++ Assets/Scripts/GameCore/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController characterController;
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
